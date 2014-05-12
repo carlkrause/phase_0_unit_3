61b7e10512f1d6976eda490160474f7a1bf045bf
carlkrause <krauseca@gmail.com>
2014-05-11 19:58:51 -0500

    jquery challenge

--- week_8_and_9/3_JavaScript/jquery/jquery_example.js
+++ week_8_and_9/3_JavaScript/jquery/jquery_example.js
@@ -7,21 +7,27 @@ $('body').css({'background-color': 'pink'})
  
 //RELEASE 1:
   //Add code here to select elements of the DOM 
- 
- 
+var bodyElement = $('body');
+$('h1')
 //RELEASE 2: 
-  // Add code here to modify the css and html of DOM elements
- 
- 
+$('h1').css('color','red');
+$('h1').css('border','dashed');
+$('h1').css('visible','hidden');
+$('.mascot h1').html("YOUR COHORT MASCOT"); 
 //RELEASE 3: Event Listener
-  // Add the code for the event listener here 
- 
- 
+$('img').on('mouseover', function(e){
+     e.preventDefault()
+    $(this).attr('src', 'http://www.evodivo.com/images/firefly2.jpg')
+  });
+
+    $('img').on('mouseleave', function(e){
+     e.preventDefault()
+    $(this).attr('src', 'dbc_logo.jpg')
+  });
 //RELEASE 4 : Experiment on your own
- 
- 
- 
- 
- 
+ $('img').on('mouseover', function(e){
+     e.preventDefault()
+    $(this).animate({height:'300px'},'slow')
+  });
  
 })  // end of the document.ready function: do not remove or write DOM manipulation below this.
